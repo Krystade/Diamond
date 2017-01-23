79bed98e67b2d9813969301e939375cc77bc821e
Krys <jackpschmid@gmail.com>
2017-01-23 13:36:40 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-
+print('How many lines wide in the center?')
+try:
+    width = int(input())
+except ValueError:
+    print('Not an Integer')
+    quit()
+for i in range(1,width,2):
+    print (('*' * i).center(width))
+center = i
+for i in range(center - 2,0,-2):
+    print (('*' * i).center(width))
